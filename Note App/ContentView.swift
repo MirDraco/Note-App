ef7b8e7c5912f2416b26db8bee95b507732854ba
Mir__Draco <furrymir09875@gmail.com>
2024-05-29 22:46:39 +0900

    Initial Commit

---
+++ Note App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Note App
+//
+//  Created by 미르 on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Note App/Note_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_AppApp.swift
+//  Note App
+//
+//  Created by 미르 on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Note_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
